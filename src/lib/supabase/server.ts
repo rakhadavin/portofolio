9120a7e60a0a12ea0b762cf7dc16275faaec27b3
rakhadavin <rakha.davin@ui.ac.id>
2026-04-21 13:54:28 +0700

    fix: error supabase server

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,68 +1,19 @@
-
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+export async function createClient() {
+  const cookieStore = await cookies();
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
-  return createServerClient(
-    supabaseUrl!,
-    supabaseKey!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll()
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
+  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
+      },
+      setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+        } catch {}
       },
     },
-  );
-};
-
-
-
-// import { createServerClient, type CookieOptions } from '@supabase/ssr'
-// import { cookies } from 'next/headers'
-
-// export function createClient() {
-//   const cookieStore = cookies()
-
-//   return createServerClient(
-//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-//     {
-//       cookies: {
-//         get(name: string) {
-//           return cookieStore.get(name)?.value
-//         },
-//         set(name: string, value: string, options: CookieOptions) {
-//           try {
-//             cookieStore.set({ name, value, ...options })
-//           } catch (error) {
-//             // The `set` method was called from a Server Component.
-//             // This can be ignored if you have middleware refreshing
-//             // user sessions.
-//           }
-//         },
-//         remove(name: string, options: CookieOptions) {
-//           try {
-//             cookieStore.set({ name, value: '', ...options })
-//           } catch (error) {
-//             // The `delete` method was called from a Server Component.
-//             // This can be ignored if you have middleware refreshing
-//             // user sessions.
-//           }
-//         },
-//       },
-//     }
-//   )
-// }
+  });
+}
